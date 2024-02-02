a9fe8aaa7a65f863f0bac1be6b2b743783ef65eb
Danilo Soares <deudzdev@gmail.com>
2024-02-02 02:22:07 -0300

    make kitty the default terminal
    i added kitty's configuration and also made it
    the default in awesome

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -45,7 +45,7 @@ end
 beautiful.init(gears.filesystem.get_configuration_dir() .. "theme.lua")
 
 -- This is used later as the default terminal and editor to run.
-terminal = "urxvt"
+terminal = "kitty"
 
 -- Default modkey.
 -- Usually, Mod4 is the key with a logo between Control and Alt.
